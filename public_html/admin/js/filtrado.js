85b5f94bc6b61446646bbe53927318bf5449e646
basededatoescuela <140933174+luisabelherrera@users.noreply.github.com>
2024-03-11 22:24:46 -0500

    proyecto html programacion 4,2

--- public_html/admin/js/filtrado.js
+++ public_html/admin/js/filtrado.js
@@ -1,34 +1,58 @@
 function filtrarTabla() {
     var input, filter, table, tr, td, i, txtValue;
 
-
     input = document.getElementById("filtroInput");
     filter = input.value.toUpperCase();
+    table = document.getElementById("tablaProfesores");
+    table = document.getElementById("tablaNotas");
+    table = document.getElementById("tablaMatriculas");
+    table = document.getElementById("tablaCursos");
+    table = document.getElementById("tablaAlumnos");
     table = document.getElementById("tablaUEA");
     tr = table.getElementsByTagName("tr");
 
-    var found = false;
+
+
 
     for (i = 0; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td");
+
         if (td.length > 0) {
             var selectedFilter = document.getElementById("filtroSelect").value;
-            var tdContent = td[0].textContent || td[0].innerText;
+            var tdContentIDProfesor = td[0].textContent || td[0].innerText;
+            var tdContentIDUsuario = td[1].textContent || td[1].innerText;
+            var tdContentIDnota = td[0].textContent || td[0].innerText;
+            var tdContentIDmatricula = td[0].textContent || td[0].innerText;
+            var tdContentIDNombre_curso = td[0].textContent || td[0].innerText;
+            var tdContentIDNombrealumno = td[0].textContent || td[0].innerText;
+            var tdContentrolCC = td[0].textContent || td[0].innerText;
 
-            if (selectedFilter === 'nombre') {
-                txtValue = td[1].textContent || td[1].innerText;
-            } else {
-                txtValue = tdContent;
+            if (selectedFilter === 'ID_USUARIO') {
+                txtValue = tdContentIDProfesor;
+            } else if (selectedFilter === 'ID_NOMBRE') {
+                txtValue = tdContentIDUsuario;
+            } else if (selectedFilter === 'ID_Alumno') {
+                txtValue = tdContentIDnota;
+
+            } else if (selectedFilter === 'ID_Matricula') {
+                txtValue = tdContentIDmatricula;
+
+            } else if (selectedFilter === 'Nombre_curso') {
+                txtValue = tdContentIDNombre_curso;
+
+            } else if (selectedFilter === 'nombrealumno') {
+                txtValue = tdContentIDNombrealumno;
+
+            } else if (selectedFilter === 'CC') {
+                txtValue = tdContentrolCC;
             }
 
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-                found = true;
+                tr[i].style.display = "table-row";
             } else {
                 tr[i].style.display = "none";
             }
-
-
         }
     }
+
 }
